6cb1b6e17d08dd135ad1d7ac3c321c3b1eac33af
Michael Richins <mr.grigri@gmail.com>
2025-09-01 16:10:26 -0600

    Fix: html structure for details + summary

--- src/routes/(no-auth)/register/+page.svelte
+++ src/routes/(no-auth)/register/+page.svelte
@@ -109,23 +109,25 @@
 
 		<details id="rules" open>
 			<summary>Password must meet the following conditions:</summary>
-			{#each rules as rule}
-				<p role="presentation">
-					{#if passwordErrors.includes(rule.key) || !passwordInputIsDirty}
-						<svg viewBox="0 0 24 24" width="24" height="24" fill="currentColor"
-							><path
-								d="M19,6.41L17.59,5L12,10.59L6.41,5L5,6.41L10.59,12L5,17.59L6.41,19L12,13.41L17.59,19L19,17.59L13.41,12L19,6.41Z"
-							/></svg
-						>
-					{:else}
-						<svg viewBox="0 0 24 24" width="24" height="24" fill="currentColor"
-							><path d="M21,7L9,19L3.5,13.5L4.91,12.09L9,16.17L19.59,5.59L21,7Z" /></svg
-						>
-					{/if}
+			<div>
+				{#each rules as rule}
+					<p role="presentation">
+						{#if passwordErrors.includes(rule.key) || !passwordInputIsDirty}
+							<svg viewBox="0 0 24 24" width="24" height="24" fill="currentColor"
+								><path
+									d="M19,6.41L17.59,5L12,10.59L6.41,5L5,6.41L10.59,12L5,17.59L6.41,19L12,13.41L17.59,19L19,17.59L13.41,12L19,6.41Z"
+								/></svg
+							>
+						{:else}
+							<svg viewBox="0 0 24 24" width="24" height="24" fill="currentColor"
+								><path d="M21,7L9,19L3.5,13.5L4.91,12.09L9,16.17L19.59,5.59L21,7Z" /></svg
+							>
+						{/if}
 
-					<span>{rule.errorDescription}</span>
-				</p>
-			{/each}
+						<span>{rule.errorDescription}</span>
+					</p>
+				{/each}
+			</div>
 		</details>
 
 		<p id="ruleStatus" role="alert" aria-live="polite" class="visually-hidden">
