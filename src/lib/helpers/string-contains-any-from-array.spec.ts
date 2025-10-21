1d6c5f7f46ac69c8b872c5cb5524552630011f60
Michael Richins <mr.grigri@gmail.com>
2025-10-21 12:34:30 -0600

    Fix: Test possible collision

--- src/lib/helpers/string-contains-any-from-array.spec.ts
+++ src/lib/helpers/string-contains-any-from-array.spec.ts
@@ -4,7 +4,7 @@ import { faker } from '@faker-js/faker';
 
 describe('stringContainsAnyFromArray', () => {
 	it('Should return false', () => {
-		const fakeString = faker.word.words(5);
+		const fakeString = faker.string.alphanumeric(5);
 		const fakeArray = [faker.word.words(1), faker.word.words(1), faker.word.words(1)];
 
 		const response = stringContainsAnyFromArray(fakeString, fakeArray);
